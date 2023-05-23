fc55056afc15c1abe5edaae818ce610d5b965e76
SebastianG302 <johan.guerrero@pi.edu.co>
2023-05-23 15:10:41 -0500

    register page started and validators.ts created

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { cantBeStrider } from 'src/app/shared/validators/validators';
 
 @Component({
   selector: 'app-register-page',
@@ -7,5 +9,18 @@ import { Component } from '@angular/core';
   ]
 })
 export class RegisterPageComponent {
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required]],
+    email: ['', [Validators.required]],
+    username: ['', [Validators.required, cantBeStrider]], //solo se pasa la referencia a la funcion para que angular la ejecute
+    password: ['', [Validators.required, Validators.minLength(6)]],
+    password2: ['', [Validators.required, Validators.minLength(6)]],
+  });
 
+  constructor(private fb: FormBuilder){}
+
+  isValidFiel(field: string){
+
+  }
+  onSubmit(){}
 }
