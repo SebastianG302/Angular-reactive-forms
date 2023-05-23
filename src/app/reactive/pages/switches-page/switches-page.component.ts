6eb8cfd5ac0f5ae798855af4cb05c62f70cb073b
SebastianG302 <johan.guerrero@pi.edu.co>
2023-05-23 13:02:10 -0500

    switches page started

--- src/app/reactive/pages/switches-page/switches-page.component.ts
+++ src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-switches-page',
@@ -7,5 +8,25 @@ import { Component } from '@angular/core';
   ]
 })
 export class SwitchesPageComponent {
+  constructor(private fb: FormBuilder){}
 
+  public myForm: FormGroup = this.fb.group({
+    gender: ['M', Validators.required],
+    wantNotifications: [true, Validators.required],
+    termsAndConditions: [false, Validators.requiredTrue]//tiene que ser verdadero
+  });
+
+  onSave(){
+    if(this.myForm.invalid){
+      this.myForm.markAllAsTouched();
+      return;
+    }
+    console.log(this.myForm.value);
+    this.myForm.reset()
+    
+  }
+
+  isValidField(field: string): boolean | null {
+    return this.myForm.controls[field].errors && this.myForm.controls[field].touched
+  }
 }
