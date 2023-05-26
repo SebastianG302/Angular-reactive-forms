e8f5a50f29691490b7008dece7d6faa36e54490e
SebastianG302 <johan.guerrero@pi.edu.co>
2023-05-26 12:35:29 -0500

    validatorService added

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ValidatorsService } from 'src/app/shared/service/validators.service';
 import { cantBeStrider, emailPattern, firstNameAndLastnamePattern } from 'src/app/shared/validators/validators';
 
 @Component({
@@ -10,17 +11,17 @@ import { cantBeStrider, emailPattern, firstNameAndLastnamePattern } from 'src/ap
 })
 export class RegisterPageComponent {
   public myForm: FormGroup = this.fb.group({
-    name: ['', [Validators.required, Validators.pattern(firstNameAndLastnamePattern)]],
-    email: ['', [Validators.required, Validators.pattern(emailPattern)]],
+    name: ['', [Validators.required, Validators.pattern(this.validatorsService.firstNameAndLastnamePattern)]],
+    email: ['', [Validators.required, Validators.pattern(this.validatorsService.emailPattern)]],
     username: ['', [Validators.required, cantBeStrider]], //solo se pasa la referencia a la funcion para que angular la ejecute
     password: ['', [Validators.required, Validators.minLength(6)]],
     password2: ['', [Validators.required, Validators.minLength(6)]],
   });
 
-  constructor(private fb: FormBuilder){}
+  constructor(private fb: FormBuilder, private validatorsService: ValidatorsService){}
 
-  isValidFiel(field: string){
-
+  isValidField(field: string){
+    return this.validatorsService.isValidField(this.myForm, field);
   }
   onSubmit(){}
 }
